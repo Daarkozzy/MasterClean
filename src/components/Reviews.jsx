46d944889127c0586b92671fe6f4128c6ac184a6
daarkozzy <daark.ozzy29@gmail.com>
2026-06-16 15:55:45 -0300

    Remove setas do carousel de avaliações

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Star, CaretLeft, CaretRight } from '@phosphor-icons/react';
+import { Star } from '@phosphor-icons/react';
 import './Reviews.css';
 
 const reviews = [
@@ -26,14 +26,6 @@ export default function Reviews() {
     return () => clearInterval(interval);
   }, []);
 
-  const goToPrevious = () => {
-    setCurrentIndex((prev) => (prev - 1 + reviews.length) % reviews.length);
-  };
-
-  const goToNext = () => {
-    setCurrentIndex((prev) => (prev + 1) % reviews.length);
-  };
-
   return (
     <section className="reviews section-padding">
       <div className="container">
@@ -47,10 +39,6 @@ export default function Reviews() {
         </header>
 
         <div className="reviews__carousel">
-          <button className="reviews__arrow reviews__arrow--left" onClick={goToPrevious} aria-label="Anterior">
-            <CaretLeft size={32} weight="fill" />
-          </button>
-
           <div className="reviews__carousel-container">
             <img 
               src={reviews[currentIndex].image} 
@@ -58,10 +46,6 @@ export default function Reviews() {
               className="reviews__carousel-image"
             />
           </div>
-
-          <button className="reviews__arrow reviews__arrow--right" onClick={goToNext} aria-label="Próximo">
-            <CaretRight size={32} weight="fill" />
-          </button>
         </div>
 
         <div className="reviews__indicators">
